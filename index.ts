30ccc181db3be44f0c660f4e1ed7594bfad8b2fa
stack72 <public@paulstack.co.uk>
2020-12-04 18:09:19 +0000

    Adding a resource

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
+
+const x = new random.RandomPassword("x", {
+  length: 30,
+  special: true,
+});
+
+export const xVal = x.result;
